8e83e06e44a6ad9ca0f1783f3c6aa4eae5bcf6b8
leeh24 <leeh24@up.edu>
2021-03-31 14:51:33 -0700

    Creating MoveAction class.

---
+++ app/src/test/java/com/example/mancala/MancalaTest.java
@@ -0,0 +1,10 @@
+package com.example.mancala;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+public class MancalaTest {
+}
