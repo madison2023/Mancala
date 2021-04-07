9fdb139856cf85458212b383b336c887a15ba8a7
madison2023 <78463010+madison2023@users.noreply.github.com>
2021-04-07 15:28:49 -0700

    changed checkIfGameOver to use player0 and player1 instead of computer and human

--- app/src/main/java/Mancala/MancalaLocalGame.java
+++ app/src/main/java/Mancala/MancalaLocalGame.java
@@ -40,23 +40,23 @@ public class MancalaLocalGame extends LocalGame {
 
         MancalaGameState state = (MancalaGameState) super.state;
 
-        int computerRowValue = 0; // number of marbles in computer pockets
-        int humanRowValue = 0;
+        int player1TotalMarbles = 0; // number of marbles in computer pockets
+        int player0TotalMarbles = 0;
 
         int[] temp = state.getPlayer0();
-        int computerStoreValue = temp[6]; // set computer score
+        int player0StoreValue = temp[6]; // set player 0 score
         for (int i = 0; i < temp.length - 1; i++) {
-            computerRowValue = temp[i] + computerRowValue;
+            player0TotalMarbles = temp[i] + player0TotalMarbles;
         }
 
         temp = state.getPlayer1();
-        int humanStoreValue = temp[6]; // set human score
+        int player1StoreValue = temp[6]; // set player 1 score
         for (int i = 0; i < temp.length - 1; i++) {
-            humanRowValue = temp[i] + humanRowValue;
+            player1TotalMarbles = temp[i] + player1TotalMarbles;
         }
 
-        if(computerRowValue == 0 || humanRowValue == 0){
-            if(humanStoreValue > computerStoreValue){
+        if(player0TotalMarbles == 0 || player1TotalMarbles == 0){
+            if(player0StoreValue > player1StoreValue){
                 return playerNames[0] + " won! ";
             } else {
                 return playerNames[1] + " won! ";
